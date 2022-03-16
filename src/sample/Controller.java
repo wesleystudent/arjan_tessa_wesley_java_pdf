e3f9db20f722710a135c9386bd60decfe1128086
tesramaker <tessa.ramaker@student.nhlstenden.com>
2022-03-16 17:19:53 +0100

    Update Frontend

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,6 @@
 package sample;
 
+import javafx.scene.control.Menu;
+
 public class Controller {
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,40 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ToolBar;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Hello World");
 
+        ImageView brush = new ImageView("res/img/brush.png");
+        brush.maxWidth(20);
+        brush.maxHeight(20);
 
+        VBox vBox = new VBox(root);
 
-        ToolBar toolBar = new ToolBar();
-
-        MenuButton menuButton = new MenuButton("File");
-        menuButton.getItems().add(new MenuItem("Save"));
-        toolBar.getItems().add(menuButton);
-
-        VBox vBox = new VBox(toolBar);
-
-
-
-        Scene scene = new Scene(vBox, 960, 600);
+        Scene scene = new Scene(vBox, 1280, 720);
         primaryStage.setScene(scene);
+
         primaryStage.show();
-
-
-
     }
 
 
