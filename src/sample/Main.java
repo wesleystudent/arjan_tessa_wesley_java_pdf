9509d04c0f6afa1c575f713c259dd9e8976be75e
tesramaker <tessa.ramaker@student.nhlstenden.com>
2022-03-24 19:53:09 +0100

    Update front-end

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,27 +1,28 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
 
 import javax.swing.*;
+import java.io.File;
 import java.util.Objects;
 
 public class Main extends Application {
+
+    @FXML
+    ImageView imageView;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Aantekeningen");
-
-        ImageView brush = new ImageView("res/img/brush.png");
-        brush.maxWidth(20);
-        brush.maxHeight(20);
-
 
         VBox vBox = new VBox(root);
 
@@ -30,6 +31,7 @@ public class Main extends Application {
 
         primaryStage.show();
     }
+
 
 
     public static void main(String[] args) {
